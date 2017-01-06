af83e1aceaebef2af19dfc5258fe9f4c9b1a8842
guokan <guokan@yinhai.com>
2017-01-06 15:42:49 +0800

    ...

--- js/distPicker.js
+++ js/distPicker.js
@@ -235,38 +235,3 @@ $(function(){
 		}
 	})
 })
-
-
-/****************************************************
- 使用方法,示例 
- HTML:
- 	DOM:
- 		//引入js、css
- 		<script type="text/javascript" src=".../distPicker.js"></script>
- 		<link  rel="stylesheet" type="text/css" href=".../distPicker-default.css">
- 		//input元素className包含"ta-distPicker"即可,url:数据来源,panelwidth:宽默认100%,panelheight:默认200px
-		<input id="a0114" class="ta-distPicker" type="text" url="userManageAction!distPicker.do" panelwidth="100%" panelheight="200px"/>
-	Ta3:
-		<ta:distPicker id="b0140" key="单位所属区划" url="companyInfoManagerAction!distPicker.do" />
- Action:
- 	public String distPicker() throws Exception{
-        ParamDTO dto=getDto();
-        String frag=dto.getAsString("frag");
-        Pattern p=ValidateUtil.isEmpty(dto.getAsString("wholeReg"))?
-                  Pattern.compile("^"+frag+"((0[1-9])|([1-9][0-9]))0*$"):
-                  Pattern.compile(frag);  
-        List<Map<String,Object>> resultList=new ArrayList();
-        List<AppCode> codeList=CodeTableLocator.getCodeList("A0114");
-        Map map;
-        for (AppCode appCode : codeList) {
-            if(p.matcher((appCode.getCodeValue())).matches()){
-               map=new HashMap();
-               map.put("id", appCode.getCodeValue());
-               map.put("name", appCode.getCodeDESC());
-               resultList.add(map);
-            }
-        }
-        setData("items",resultList);
-        return JSON;
-    }
-*/
